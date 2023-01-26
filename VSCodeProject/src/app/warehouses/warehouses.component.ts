c7c32c8f0e760cae65f4e49effd7f71716c799c5
Victoria Morton <104038505+TorMorton@users.noreply.github.com>
2023-01-26 15:36:24 -0500

    ershb

--- VSCodeProject/src/app/warehouses/warehouses.component.ts
+++ VSCodeProject/src/app/warehouses/warehouses.component.ts
@@ -1,29 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators} from '@angular/forms';
 import { Product } from '../models/product';
 import { CRUDService } from '../services/crud.service';
 import { ProductService } from '../services/product.service';
 import { Observable } from 'rxjs';
+import { WarehouseService } from '../services/warehouse.service';
+import { Warehouse } from '../models/warehouse';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-warehouses',
   templateUrl: './warehouses.component.html',
   styleUrls: ['./warehouses.component.css']
 })
-export class WarehousesComponent{
+export class WarehousesComponent implements OnInit{
 
   // class properties
   isCollapsed: boolean = true;
   inventory: any = [];
   tempInventory: any;
+  totalInStock: number = 0;
+  limit = 100 - this.totalInStock;
+  warehouses: Warehouse[] = this.warehouseService.warehouses;
+  nameId: number = 0;
+
 
   // constructor
 
-  constructor(private crudService: CRUDService, private productService: ProductService, private fb: FormBuilder,) {
-    this.crudService.getAll().subscribe(data => {
-      console.log(data);
-      this.inventory = data.body
-  })
+  constructor(private crudService: CRUDService, private productService: ProductService, private warehouseService: WarehouseService, private fb: FormBuilder,) {
+  //   this.crudService.getAll().subscribe(data => {
+  //     console.log(data);
+  //     this.inventory = data.body
+  // })
   }
 
 
@@ -33,6 +41,7 @@ export class WarehousesComponent{
       manufacturer: ['', Validators.compose([Validators.maxLength(50), Validators.required])],
       model: ['', Validators.compose([Validators.maxLength(50), Validators.required])],
       price: [0, Validators.compose([Validators.required])],
+      numInStock: [1, Validators.compose([Validators.required, Validators.max(this.limit)])]
     }
   );
 
@@ -48,27 +57,52 @@ export class WarehousesComponent{
     return this.productForm.get('price');
   }
 
+  get numInStock() {
+    return this.productForm.get('numInStock');
+  }
+
   tempProduct: any = {
     manufacturer: 'manufacturer',
     model: 'model',
-    cost: 0 
+    cost: 0,
+    numInStock: 0
+  };
+
+  tempWarehouse: any = {
+    name: this.warehouseService.warehouses[this.nameId].warehouseName,
+    location: this.warehouseService.warehouses[this.nameId].location,
+    capacity: 100,
+    currentTotal: 0
   };
 
   tempProductId: number = 0;
 
 
   // methods
-  onInIt() {
+  ngOnInit(): void {
     this.displayAll();
+
+    switch (sessionStorage.getItem("warehouseURL")) {
+      case ("gpu_inventory/") :
+        this.nameId = 0;
+        break;
+      case ("cpu_inventory/") :
+        this.nameId = 1;
+        break;
+      case ("psu_inventory/") :
+        this.nameId = 2;
+        break;
+    }    
   }
 
   displayAll() {
     this.crudService.getAll().subscribe(data => {
       console.log(data);
       this.inventory = data.body;
+      console.log(this.inventory);
+      this.countCurrentTotal();
   })
     this.tempInventory = null;
-    console.log('displaAll() ran')
   }
 
   displayById(id: number) {
@@ -88,6 +122,7 @@ export class WarehousesComponent{
         this.tempProduct.model = item.model;
         this.tempProduct.cost = item.cost;
         this.tempProduct.id = id;
+        this.tempProduct.numInStock = item.numInStock;
         break;        
       }
     }
@@ -96,25 +131,31 @@ export class WarehousesComponent{
   onSubmit(): void {
     this.productService.createProduct(new Product(this.manufacturer?.value!, 
                                                   this.model?.value!, 
-                                                  this.price?.value!));
+                                                  this.price?.value!,
+                                                  this.numInStock?.value!));
     this.tempProduct.manufacturer = this.productService.product.manufacturer;
     this.tempProduct.model = this.productService.product.model;
     this.tempProduct.cost = this.productService.product.cost;
+    this.tempProduct.numInStock = this.productService.product.numInStock;
   }
 
   save() {
+    this.countCurrentTotal();
     this.crudService.save(this.tempProduct).subscribe(data => {
       console.log(data);
       this.displayAll();
+      
     });
   }
   
   update() {
     console.log('inside warehouse update()')
-    this.crudService.update(this.tempProduct, this.tempProduct.id).subscribe(data => {
-      console.log(data);
-      this.displayAll();
-    });
+    if(this.tempProduct.numInStock + this.totalInStock <= this.warehouseService.warehouses[this.nameId].capacity ){
+      this.crudService.update(this.tempProduct, this.tempProduct.id).subscribe(data => {
+        console.log(data);
+        this.displayAll();
+      });
+    }
   }
 
   deleteById() {
@@ -133,5 +174,25 @@ export class WarehousesComponent{
     }
     this.displayAll();
   }
-  
+
+  countCurrentTotal(): void {
+    console.log('run currentTotal')
+    this.totalInStock = 0;
+    for (let item of this.inventory) {
+      this.totalInStock += item.numInStock;
+    }
+    this.tempWarehouse.currentTotal = this.totalInStock;
+    this.crudService.updateCurrentTotal(this.tempWarehouse, this.nameId + 1).subscribe(data => {
+      console.log(data)
+    })
+  }
+
+  onEdit(item: any) {
+    item.isEdit = true;
+   }
+
+   updateProduct(item: { isEdit: boolean;}) {
+    item.isEdit = false;
+  } 
+
 }
